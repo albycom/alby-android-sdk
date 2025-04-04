d507244b474de98cbf1c046dd3aa4f07b8bf6cfc
jasondeng <jason.deng@bluecore.com>
2025-04-04 15:21:32 -0400

    chore: update library version to 1.0.28 in gradle.properties, switch to maven-publish plugin in build.gradle.kts, and refine release workflow publish command

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,4 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.vanniktech.maven.publish") version "0.25.0" apply false
 }
