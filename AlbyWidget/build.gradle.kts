0cc37a50d364bedbb05138be89951a2c8655fcca
jasondeng <jason.deng@bluecore.com>
2025-04-04 15:08:46 -0400

    chore: update build configuration to use com.vanniktech.maven.publish plugin and simplify release workflow publish command

--- AlbyWidget/build.gradle.kts
+++ AlbyWidget/build.gradle.kts
@@ -1,11 +1,8 @@
-import java.util.Base64
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -61,49 +58,4 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.okhttp)
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = findProperty("GROUP") as String
-                artifactId = findProperty("POM_ARTIFACT_ID") as String
-                version = findProperty("VERSION_NAME") as String
-
-                afterEvaluate {
-                    from(components["release"])
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                name = "mavenCentral"
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                
-                val tokenId = findProperty("mavenCentralUsername")?.toString() ?: return@maven
-                val tokenSecret = findProperty("mavenCentralPassword")?.toString() ?: return@maven
-                val encoded = Base64.getEncoder().encodeToString("$tokenId:$tokenSecret".toByteArray())
-
-                credentials(HttpHeaderCredentials::class) {
-                    name = "Authorization"
-                    value = "Basic $encoded"
-                }
-
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            }
-        }
-    }
-
-    signing {
-        val keyId = findProperty("signingInMemoryKeyId")?.toString() ?: return@signing
-        val key = findProperty("signingInMemoryKey")?.toString() ?: return@signing
-        val keyPassword = findProperty("signingInMemoryKeyPassword")?.toString() ?: return@signing
-        
-        sign(publishing.publications["maven"])
-        useInMemoryPgpKeys(keyId, key, keyPassword)
-    }
 }
